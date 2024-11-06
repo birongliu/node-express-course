8d42d8cb28173c9c654b5f14bdf0b0ac7cbb74ee
birongliu <birongliu2003@gmail.com>
2024-11-06 11:34:07 -0500

    fix typo in back home link text in HTTP tutorial answer

--- 01-node-tutorial/answers/12-http.js
+++ 01-node-tutorial/answers/12-http.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
   res.end(`
     <h1>Oops!</h1>
   <p>We can't seem to find the page you are looking for</p>
-  <a href="/">back home</a>
+  <a href="/">back home!</a>
     `);
   // ###################################
   // ###################################
