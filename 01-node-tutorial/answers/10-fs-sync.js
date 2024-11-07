b3df5c53e7187ec2ed6075bb4ccb0603f8e0422f
birongliu <birongliu2003@gmail.com>
2024-11-07 00:11:08 -0500

    update file writing methods in Node.js tutorial answers to append content and improve error handling

--- 01-node-tutorial/answers/10-fs-sync.js
+++ 01-node-tutorial/answers/10-fs-sync.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const filePath = './temporary/fileA.txt';
 
-fs.writeFileSync(filePath, 'This is the first line.\n');
+fs.writeFileSync(filePath, 'This is the first line.\n', { flag: 'a' });
 
 fs.writeFileSync(filePath, 'This is the second line.\n', { flag: 'a' });
 fs.writeFileSync(filePath, 'This is the third line.\n', { flag: 'a' });
